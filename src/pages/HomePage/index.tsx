399cbda508aad4efefb3bdd11a4c979f99b801d4
Buse Karasakal <buseozvtn@gmail.com>
2023-01-29 16:47:00 +0100

    Fix: pagination

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -3,40 +3,29 @@ import { CharacterService } from '../../services/CharacterService';
 import { useState } from 'react';
 import ReactPaginate from 'react-paginate';
 
-const ITEMS_PER_PAGE = 4;
-
 export function HomePage() {
-  const [itemOffset, setItemOffset] = useState(0);
-
-  const characterList = CharacterService.useGetCharacterList().characterList;
-  const endOffset = itemOffset + ITEMS_PER_PAGE;
-  const currentItems = characterList.slice(itemOffset, endOffset);
-  const pageCount = Math.ceil(characterList.length / ITEMS_PER_PAGE);
-
-  const handlePageClick = (event) => {
-    const newOffset = (event.selected * ITEMS_PER_PAGE) % characterList.length;
-    setItemOffset(newOffset);
-  };
+  const [page, setPage] = useState<number>(1);
+  const { characterList, listInfo } = CharacterService.useGetCharacterList(page);
 
   return (
     <>
       <div className='flex flex-col lg:flex-row lg:flex-wrap lg:justify-between lg:content-center lg:p-16 '>
-        {currentItems.map(({ id, name, image, species, status, location }) => (
+        {characterList.map(({ id, name, image, species, status, location }) => (
           <CardList key={`card-${id}`} title={name} image={image} description={location.name} status={status}
                     type={species} />
         ))}
       </div>
       <div className='flex justify-center items-end w-auto lg:w-full h-36 pt-24'>
         <ReactPaginate
-          className='flex lg:gap-20 gap-8 h-full text-center items-center cursor-point shadow-lg shadow-zinc-700/50 text-white'
-          activeClassName='text-xl font-bold text-lime-500'
+          className='flex lg:gap-20 gap-8 h-full text-center items-center lg:mb-6 cursor-point shadow-lg shadow-zinc-700/50 text-white'
+          activeClassName='lg:text-xl font-bold text-lime-500'
           breakLabel='...'
           nextLabel=' >'
-          onPageChange={handlePageClick}
-          pageRangeDisplayed={5}
-          pageCount={pageCount}
+          onPageChange={() => setPage((prevState) => prevState + 1)}
+          pageRangeDisplayed={2}
+          pageCount={listInfo!.pages | 1}
+          marginPagesDisplayed={0}
           previousLabel='< '
-          renderOnZeroPageCount={null}
         />
       </div>
     </>
