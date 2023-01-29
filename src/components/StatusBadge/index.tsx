e6426e70fa57f3ec36c7411a51ad33e158d67160
Buse Karasakal <buseozvtn@gmail.com>
2023-01-29 22:29:41 +0100

    Fix: status badge

--- src/components/StatusBadge/index.tsx
+++ src/components/StatusBadge/index.tsx
@@ -2,14 +2,14 @@ import { STATUS } from '../../types/status.enum';
 import { StatusType } from '../../types/status.type';
 
 export function StatusBadge({ status, type }: StatusType) {
-  const statusColor = (status) => {
+  const statusColor = (status: string) => {
     return status === STATUS.Alive ? 'bg-lime-500' : status === STATUS.Dead ? 'bg-black' : 'bg-slate-200';
   };
 
   return (
     <div className='items-center pt-3 pb-3'>
       <div className={`inline-flex w-2 h-2 mr-1.5 rounded-full ${statusColor(status)}`}></div>
-      <span className='text-white'>{STATUS[status]} - {type}</span>
+      <span className='text-white'>{status.toUpperCase()} - {type}</span>
     </div>
   );
 }
